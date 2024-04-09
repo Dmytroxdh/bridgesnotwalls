83a23a9fbb543397cb7af11ca75441c10bf1560a
Matt Ferdinandus <mferdinandus@outlook.com>
2024-04-09 13:02:46 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,18 @@
+import express from "express";
+import bodyParser from "body-parser";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+
+const app = express();
+const port = 3000;
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.listen(3000, () =>{
+    console.log(`server listening on port ${port}`);
+});
+
+app.get("/", (req, res) =>{
+    res.sendFile(__dirname + "/Public/index.html");
+});
